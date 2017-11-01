a213bde7cd700b9faf86807b87f5acae5701e865
ljsachs <larry.j.sachs@intel.com>
2017-10-31 17:20:59 -0700

    Delete setenv.sh

--- alexa-iotivity-bridge-demo/setenv.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH="/home/larrys/work/iotivity-1.3-rel/iotivity/out/linux/x86_64/release/"
-#export LD_LIBRARY_PATH="/home/larrys/work/iotivity-1.3-rel/iotivity/out/linux/x86_64/debug/"
